1f46cc98cf77e0570538d3037f3314720932e04f
Geraldo Santos <geraldo.gsantos12@gmail.com>
2024-05-06 16:22:10 -0300

    criado os botões adicionar e salvar, suas funções e chamada da função

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ co1 = "#feffff"  # Branca
 co2 = "#e5e5e5"  # grey
 co3 = "#00a095"  # Verde
 co4 = "#403d3d"   # letra
-co6 = "#003452"   # azul
-co7 = "#ef5350"   # vermelha
+co5 = "#003452"   # azul
+co6 = "#ef5350"   # vermelha
 
-co6 = "#038cfc"   # azul
+co7 = "#038cfc"   # azul
 co8 = "#263238"   # + verde
 co9 = "#e9edf5"   # + verde
 
@@ -32,7 +32,7 @@ style = Style(janela)
 style.theme_use("clam")
 
 # Criando Frames
-frame_logo = Frame(janela, width=850, height=52, bg=co6)
+frame_logo = Frame(janela, width=850, height=52, bg=co5)
 frame_logo.grid(row=0, column=0, pady=0, padx=0, sticky=NSEW)
 
 ttk.Separator(janela, orient=HORIZONTAL).grid(row=1, columnspan=1, ipadx=680)
@@ -53,12 +53,77 @@ frame_tabela.grid(row=5, column=0, pady=0, padx=10, sticky=NSEW)
 app_lg = Image.open('allianz.png')
 app_lg = app_lg.resize((50,50))
 app_lg = ImageTk.PhotoImage(app_lg)
-app_logo = Label(frame_logo, image=app_lg, text="Allianz Jiu Jitsu Itaquera", width=850, compound=LEFT, relief=RAISED, anchor=NW, font=('ivy 15 bold'), bg=co6, fg=co1)
+app_logo = Label(frame_logo, image=app_lg, text="Allianz Jiu Jitsu Itaquera", width=850, compound=LEFT, relief=RAISED, anchor=NW, font=('ivy 15 bold'), bg=co5, fg=co1)
 app_logo.place(x=0, y=0)
 
+# função para cadastrar alunos
+def alunos():
+    print('Aluno')
+
+# função para adicionar cursos e turmas
+def adicionar():
+    print('Cursos e Turmas')
+
+# função para salvar 
+def salvar():
+    print('Salvar')
 
 
+# função de controle -----------------------------------------------------
+
+def control(i):
+    # cadastro de aluno
+    if i == 'cadastro':
+        for widget in frame_detalhes.winfo_children():
+            widget.destroy()
+
+        for widget in frame_tabela.winfo_children():
+            widget.destroy()
+
+        # chamando a função aluno
+        alunos()
+
+    if i == 'adicionar':
+        for widget in frame_detalhes.winfo_children():
+            widget.destroy()
+
+        for widget in frame_tabela.winfo_children():
+            widget.destroy()
+
+        # chamando a função adicionar
+        adicionar()
+
+    if i == 'salvar':
+        for widget in frame_detalhes.winfo_children():
+            widget.destroy()
+
+        for widget in frame_tabela.winfo_children():
+            widget.destroy()
+
+        # chamando a função salvar
+        salvar()
+
+# Criando os botoes------------------------------------------------
+
+app_img_cadastro = Image.open('add.png')
+app_img_cadastro = app_img_cadastro.resize((18,18))
+app_img_cadastro = ImageTk.PhotoImage(app_img_cadastro)
+app_cadastro = Button(frame_dados, command=lambda:control('cadastro'), image=app_img_cadastro, text="Cadastro", width=100, compound=LEFT, overrelief=RIDGE , font=('ivy 11 bold'), bg=co1, fg=co0)
+app_cadastro.place(x=10, y=30)
+
+
+app_img_adicionar = Image.open('add.png')
+app_img_adicionar = app_img_adicionar.resize((18,18))
+app_img_adicionar = ImageTk.PhotoImage(app_img_adicionar)
+app_adicionar = Button(frame_dados, command=lambda:control('adicionar'), image=app_img_adicionar, text="Adicionar", width=100, compound=LEFT, overrelief=RIDGE , font=('ivy 11 bold'), bg=co1, fg=co0)
+app_adicionar.place(x=123, y=30)
+
+
+app_img_salvar = Image.open('salvar.png')
+app_img_salvar = app_img_salvar.resize((18,18))
+app_img_salvar = ImageTk.PhotoImage(app_img_salvar)
+app_salvar = Button(frame_dados, command=lambda:control('salvar'), image=app_img_salvar, text="Salvar", width=100, compound=LEFT, overrelief=RIDGE , font=('ivy 11 bold'), bg=co1, fg=co0)
+app_salvar.place(x=236, y=30)
 
 
 janela.mainloop()
-
